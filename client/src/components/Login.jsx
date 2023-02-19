9c6d645960cab4d07be08a6c843f5b476f42e218
Your Name <you@example.com>
2023-02-20 01:10:02 +0530

    initial commit

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { GoogleLogin , googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import video from '../assets/share.mp4';
+import logo from '../assets/logowhite.png'; 
+import { client } from '../client';
+import jwt_decode from "jwt-decode";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) =>{
+  
+    var decodedHeader = jwt_decode(response.credential);
+    // console.log(decodedHeader); 
+
+    localStorage.setItem('user',JSON.stringify(decodedHeader));
+    
+    const { name, sub, picture } = decodedHeader ;
+
+    const doc = {
+      _id : sub,
+      _type : 'user',
+      userName : name,
+      image : picture,
+    }
+
+    client.createIfNotExists(doc)
+    .then(() =>{
+      navigate('/',{ replace : true })
+    })  
+  }
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+            src = {video}
+            type = 'video/mp4'
+            loop
+            controls = {false}
+            muted
+            autoPlay
+            className='w-full h-full object-cover'
+        />
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5 '>
+            <img 
+              src={logo} 
+              width = '90px'
+              alt = 'logo'              
+            />
+          </div>
+          <div>
+          
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              onSuccess = {responseGoogle}
+              onError = {responseGoogle}
+            />
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Login
